b5bbae5b06800af831bf3ff572582495ad03c368
nairx <nairxprv@gmail.com>
2025-04-23 13:49:09 +0530

    Updated

--- q27.js
+++ q27.js
@@ -29,26 +29,55 @@ db.users.aggregate([
       as: "posts",
     },
   },
-  { $project: {
-    _id:0, name: 1, 
-    "posts.title": 1, 
-    "posts.desc": 1 } },
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      "posts.title": 1,
+      "posts.desc": 1,
+    },
+  },
 ]);
 
-
+db.users.aggregate([
+  {
+    $lookup: {
+      from: "posts",
+      localField: "_id",
+      foreignField: "userId",
+      as: "posts",
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      "posts.title": 1,
+      "posts.desc": 1,
+    },
+  },
+  { $match: { "posts.title": "title 1" } },
+]);
 
 db.users.aggregate([
-    {
-      $lookup: {
-        from: "posts",
-        localField: "_id",
-        foreignField: "userId",
-        as: "posts",
-      },
+  {
+    $lookup: {
+      from: "posts",
+      let: { uid: "$_id" },
+      pipeline: [
+        { $match: { $expr: { $eq: [ "$userId","$$uid" ] } } },
+        {$match:{title:"title 1"}}
+      ],
+      as: "posts",
     },
-    { $project: {
-      _id:0, name: 1, 
-      "posts.title": 1, 
-      "posts.desc": 1 } },
-      {$unwind:"$posts"}
-  ]);
+  },
+  {
+    $project: {
+      _id: 0,
+      name: 1,
+      "posts.title": 1,
+      "posts.desc": 1,
+    },
+  },
+  {$unwind:"$posts"}
+]);
