25d2f96be97cd6a10be2f0e72402b3c99e1340e3
nairx <nairxprv@gmail.com>
2025-04-25 11:22:01 +0530

    Updated

--- q30.js
+++ q30.js
@@ -7,3 +7,7 @@ start mongod -replSet rs1 -logpath d:\replica-example\data1\1.log --dbpath d:\re
 start mongod -replSet rs1 -logpath d:\replica-example\data2\2.log --dbpath d:\replica-example\data2\ --port 27019
 
 start mongod -replSet rs1 -logpath d:\replica-example\data3\3.log --dbpath d:\replica-example\data3\ --port 27020
+
+mongosh --port 27018
+
+rs.initiate({_id:"rs1",members:[{_id:0,host:"127.0.0.1:27018"},{_id:1,host:"127.0.0.1:27019"},{_id:2,host:"127.0.0.1:27020"}]})
